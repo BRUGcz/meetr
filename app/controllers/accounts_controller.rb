0b3e22d9f7cbc0ca8dd125e67cc31f662e3c8bde
Michal Fojtik <mfojtik@redhat.com>
2010-11-11 21:57:18 +0100

    Added Account controller (displaying some user details and stats)

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,18 @@
+class AccountsController < ApplicationController
+  before_filter :authenticate_user!, :except => [ :show ]
+
+  def update
+    account = current_user.account
+    account.update_attributes!(params[:account])
+    flash[:notice] = "Your profile was updated!"
+    redirect_to account_url(account.id)
+  end
+
+  def show
+    @account = current_user.account
+    respond_to do |format|
+      format.html { render :show }
+    end
+  end
+
+end
